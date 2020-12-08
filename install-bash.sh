013730882cb3a4533e7d074aeafc3a87d5073298
NetanMangal <imnetanmangal@gmail.com>
2020-12-08 12:56:28 +0530

    added colors to echo

--- install-bash.sh
+++ install-bash.sh
@@ -18,25 +18,25 @@ sudo apt-get install -y git
 sudo apt-get install -y rename
 sudo apt-get install -y xargs
 
-echo "installing bash_profile aliases from recon_profile"
+echo -e "\e[1;31m installing bash_profile aliases from recon_profile \e[0m"
 git clone https://github.com/NetanMangal/recon_profile.git
 cd recon_profiles
 cat .bash_profile >> ~/.bash_profile
 source ~/.bash_profile
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
 
 #install go
 if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
+echo -e "\e[1;31m It looks like go is not installed, would you like to install it now \e[0m"
 PS3="Please select an option : "
 choices=("yes" "no")
 select choice in "${choices[@]}"; do
         case $choice in
                 yes)
 
-					echo "Installing Golang"
+					echo -e "\e[1;31m Installing Golang \e[0m"
 					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
 					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
 					sudo mv go /usr/local
@@ -51,8 +51,8 @@ select choice in "${choices[@]}"; do
 					break
 					;;
 				no)
-					echo "Please install go and rerun this script"
-					echo "Aborting installation..."
+					echo -e "\e[1;31m Please install go and rerun this script \e[0m"
+					echo -e "\e[1;31m Aborting installation... \e[0m"
 					exit 1
 					;;
 	esac	
@@ -61,9 +61,9 @@ fi
 
 
 #Don't forget to set up AWS credentials!
-echo "Don't forget to set up AWS credentials!"
+echo -e "\e[1;31m Don't forget to set up AWS credentials! \e[0m"
 apt install -y awscli
-echo "Don't forget to set up AWS credentials!"
+echo -e "\e[1;31m Don't forget to set up AWS credentials! \e[0m"
 
 
 
@@ -72,120 +72,120 @@ mkdir ~/tools
 cd ~/tools/
 
 #install aquatone
-echo "Installing Aquatone"
+echo -e "\e[1;31m Installing Aquatone \e[0m"
 go get github.com/michenriksen/aquatone
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 #install chromium
-echo "Installing Chromium"
+echo -e "\e[1;31m Installing Chromium \e[0m"
 sudo snap install chromium
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing JSParser"
+echo -e "\e[1;31m installing JSParser \e[0m"
 git clone https://github.com/nahamsec/JSParser.git
 cd JSParser*
 sudo python setup.py install
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing Sublist3r"
+echo -e "\e[1;31m installing Sublist3r \e[0m"
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r*
 pip install -r requirements.txt
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo "installing teh_s3_bucketeers"
+echo -e "\e[1;31m installing teh_s3_bucketeers \e[0m"
 git clone https://github.com/tomdev/teh_s3_bucketeers.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo "installing dirsearch"
+echo -e "\e[1;31m installing dirsearch \e[0m"
 git clone https://github.com/maurosoria/dirsearch.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo "installing lazys3"
+echo -e "\e[1;31m installing lazys3 \e[0m"
 git clone https://github.com/nahamsec/lazys3.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing virtual host discovery"
+echo -e "\e[1;31m installing virtual host discovery \e[0m"
 git clone https://github.com/jobertabma/virtual-host-discovery.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo "installing sqlmap"
+echo -e "\e[1;31m installing sqlmap \e[0m"
 git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing knock.py"
+echo -e "\e[1;31m installing knock.py \e[0m"
 git clone https://github.com/guelfoweb/knock.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing lazyrecon"
+echo -e "\e[1;31m installing lazyrecon \e[0m"
 git clone https://github.com/nahamsec/lazyrecon.git
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing nmap"
+echo -e "\e[1;31m installing nmap \e[0m"
 sudo apt-get install -y nmap
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing massdns"
+echo -e "\e[1;31m installing massdns \e[0m"
 git clone https://github.com/blechschmidt/massdns.git
 cd ~/tools/massdns
 make
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing asnlookup"
+echo -e "\e[1;31m installing asnlookup \e[0m"
 git clone https://github.com/yassineaboukir/asnlookup.git
 cd ~/tools/asnlookup
 pip install -r requirements.txt
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing httprobe"
+echo -e "\e[1;31m installing httprobe \e[0m"
 go get -u github.com/tomnomnom/httprobe 
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing unfurl"
+echo -e "\e[1;31m installing unfurl \e[0m"
 go get -u github.com/tomnomnom/unfurl 
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing waybackurls"
+echo -e "\e[1;31m installing waybackurls \e[0m"
 go get github.com/tomnomnom/waybackurls
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "installing crtndstry"
+echo -e "\e[1;31m installing crtndstry \e[0m"
 git clone https://github.com/nahamsec/crtndstry.git
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
-echo "downloading Seclists"
+echo -e "\e[1;31m downloading Seclists \e[0m"
 cd ~/tools/
 git clone https://github.com/danielmiessler/SecLists.git
 cd ~/tools/SecLists/Discovery/DNS/
 ##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
 cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo "installing wpscan"
+echo -e "\e[1;31m installing wpscan \e[0m"
 git clone https://github.com/wpscanteam/wpscan.git
 cd wpscan*
 sudo gem install bundler && bundle install --without test
 cd ~/tools/
-echo "done"
+echo -e "\e[1;31m done \e[0m"
 
 
-echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\e[1;31m Done! All tools are set up in ~/tools \e[0m"
 ls -la
-echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
+echo -e "\e[1;36m One last time: don't forget to set up AWS credentials in ~/.aws/! \e[0m"
